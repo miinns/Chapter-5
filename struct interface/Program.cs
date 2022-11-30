4e4367fe80f60ad004acc93d29b216d48aeeae4e
함민서 <113820366+miinns@users.noreply.github.com>
2022-12-01 00:38:18 +0900

    Update Program.cs

--- struct interface/Program.cs
+++ struct interface/Program.cs
@@ -38,7 +38,7 @@ class StructImpApp
 {
     public static void Main()
     {
-        Vector a = new Vector(100); // 100 = 스택 영역
+        Vector a = new Vector(100); // 100 = 스택 영
         int n;
         while (true)
         { // 0이 입력될 때까지 반복
