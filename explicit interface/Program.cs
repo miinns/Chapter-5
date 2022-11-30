3b1671281bff3357241c397785a1bdefd285680b
함민서 <113820366+miinns@users.noreply.github.com>
2022-12-01 01:06:23 +0900

    Update Program.cs

--- explicit interface/Program.cs
+++ explicit interface/Program.cs
@@ -9,7 +9,7 @@ public interface ISurface
 }
 public class SampleClass : IControl, ISurface
 {
-    //explicit int
+    //explicit in
     void IControl.Paint()
     {
         System.Console.WriteLine("IControl.Paint");
