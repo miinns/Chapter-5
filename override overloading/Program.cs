1aeb7fe46a9af3ae349963d74995d114b5bbeb5c
함민서 <113820366+miinns@users.noreply.github.com>
2022-12-01 00:48:54 +0900

    Update Program.cs

--- override overloading/Program.cs
+++ override overloading/Program.cs
@@ -14,7 +14,7 @@ class DerivedClass : BaseClass
         Console.WriteLine("In DerivedClass ... Overriding !!!");
     }
 
-    // Overloadi
+    // Overload
     public void MethodA(int i)
     {
         Console.WriteLine("In DerivedClass ... Overloading !!!");
