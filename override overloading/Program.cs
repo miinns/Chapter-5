d2a7227321d0c28440da087fefb3304c0c0f4aa3
함민서 <113820366+miinns@users.noreply.github.com>
2022-12-01 00:48:01 +0900

    Update Program.cs

--- override overloading/Program.cs
+++ override overloading/Program.cs
@@ -27,7 +27,7 @@ class OverridingAndOverloadingApp
         BaseClass obj1 = new BaseClass();
         DerivedClass obj2 = new DerivedClass();
         obj1.MethodA();
-        obj2.MethodA(); /
+        obj2.MethodA(); 
         obj2.MethodA(1); 
     }
 }
